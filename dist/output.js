5a266cf4f8fddcc30e3bb69267d73b6c697677c7
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2023-02-07 21:05:24 +0500

    navbar and home page are completed

--- dist/output.js
+++ dist/output.js
@@ -1,24 +1,13 @@
 let hamburger = document.getElementById('hamburger');
 let bottom_navbar = document.getElementById('bottom_navbar');
 let close_remark = document.getElementById('close_remark');
-
+let ulElement_mobile = document.getElementById('ulElement-mobile');
 const hamburgerOnclick = () =>{
     bottom_navbar.classList.toggle('hidden')
-}
-const closeRemarkOnclick = () =>{
-    bottom_navbar.classList.toggle('hidden')
-}
-hamburger.onclick = () => {
-    hamburgerOnclick()
     hamburger.classList.toggle('hidden');
     close_remark.classList.toggle('hidden');
     close_remark.classList.add('duration-500');
     close_remark.classList.toggle('duration-500');
 }
-close_remark.onclick = () => {
-    hamburgerOnclick()
-    hamburger.classList.toggle('hidden');
-    close_remark.classList.toggle('hidden');
-    hamburger.classList.add('duration-500')
-    hamburger.classList.toggle('duration-500')
-}
+hamburger.onclick = () => hamburgerOnclick()
+close_remark.onclick = () => hamburgerOnclick()
